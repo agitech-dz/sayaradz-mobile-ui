adae56d4304f0044dee5888e0ba5682b8bcb9868
mohamed-amine-guerras <fm_guerras@esi.dz>
2019-07-04 16:52:19 +0100

    feat: add brand section to home fragment

---
+++ app/src/main/java/com/example/sayaradz_mobile/Brand.kt
@@ -0,0 +1,3 @@
+package com.example.sayaradz_mobile
+
+data class Brand(var brandName:String)
--- app/src/main/java/com/example/sayaradz_mobile/HomeFragment.kt
+++ app/src/main/java/com/example/sayaradz_mobile/HomeFragment.kt
@@ -1,7 +1,5 @@
 package com.example.sayaradz_mobile
 
-import android.content.Intent
-import android.graphics.ColorSpace
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -9,11 +7,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
 import android.widget.LinearLayout
 import android.widget.TextView
-import kotlinx.android.synthetic.main.car_item.view.*
-import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment : Fragment() {
@@ -22,6 +17,7 @@ class HomeFragment : Fragment() {
         val instance = HomeFragment()
     }
     private var carList = ArrayList<Car>()
+    private var brandList = ArrayList<Brand>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,16 +29,26 @@ class HomeFragment : Fragment() {
         carList.add(Car("BMW 320d","3.000.000da",""))
         carList.add(Car("BMW 320d","3.000.000da",""))
         carList.add(Car("BMW 320d","3.000.000da",""))
+        brandList.add(Brand("BMW"))
+        brandList.add(Brand("Mercedes"))
+        brandList.add(Brand("Audi"))
+        brandList.add(Brand("Bently"))
+        brandList.add(Brand("Ferari"))
+        brandList.add(Brand("Lamborghini"))
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         val recyclerView2 = view.findViewById<RecyclerView>(R.id.recyclerView2)
         val recyclerView3 = view.findViewById<RecyclerView>(R.id.recyclerView3)
+        val recyclerView4 = view.findViewById<RecyclerView>(R.id.recyclerView4)
         recyclerView.layoutManager = LinearLayoutManager(context, LinearLayout.HORIZONTAL, false)
         recyclerView2.layoutManager = LinearLayoutManager(context, LinearLayout.HORIZONTAL, false)
         recyclerView3.layoutManager = LinearLayoutManager(context, LinearLayout.HORIZONTAL, false)
+        recyclerView4.layoutManager = LinearLayoutManager(context, LinearLayout.HORIZONTAL, false)
         val rvAdapter = CarAdapter(carList)
+        val brandAdapter = BrandAdapter(brandList)
         recyclerView.adapter = rvAdapter
         recyclerView2.adapter = rvAdapter
         recyclerView3.adapter = rvAdapter
+        recyclerView4.adapter = brandAdapter
 
 
         return view
@@ -66,9 +72,33 @@ class HomeFragment : Fragment() {
 
         class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-            val carName = itemView.findViewById<TextView>(R.id.carName)
+            val carName = itemView.findViewById<TextView>(R.id.brandName)
 
             val carPrice = itemView.findViewById<TextView>(R.id.carPrice)
+        }
+
+
+    }
+
+    class BrandAdapter(var itemList:List<Brand>): RecyclerView.Adapter<BrandAdapter.ViewHolder>(){
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): BrandAdapter.ViewHolder {
+            val v = LayoutInflater.from(p0?.context).inflate(R.layout.brand_item, p0, false)
+
+            return ViewHolder(v)
+        }
+
+        override fun getItemCount(): Int {
+            return itemList.size
+        }
+
+        override fun onBindViewHolder(p0: BrandAdapter.ViewHolder, p1: Int) {
+            p0.brandName.text = itemList.get(p1).brandName
+        }
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+            val brandName = itemView.findViewById<TextView>(R.id.brandName)
+
         }
 
 
